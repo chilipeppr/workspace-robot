37f3b9579288bc235e09a62c41f57590b00a8f66
John Lauer <jlauer12@gmail.com>
2019-03-07 16:49:01 -0800

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ not conflict with other ChiliPeppr objects.
 | ID                    | com-chilipeppr-workspace-robot |
 | Name                  | Workspace / Robot |
 | Description           | This workspace is for controlling a 6 DOF robot arm. |
-| chilipeppr.load() URL | http://raw.githubusercontent.com/https://master/auto-generated-workspace.html |
-| Edit URL              | http://ide.c9.io/undefined/undefined |
-| Github URL            | http://github.com/https:/ |
-| Test URL              | https://preview.c9users.io/undefined/undefined/workspace.html |
+| chilipeppr.load() URL | https://raw.githubusercontent.com/chilipeppr/workspace-robot/master/auto-generated-workspace.html |
+| Edit URL              | undefined |
+| Github URL            | https://github.com/chilipeppr/workspace-robot |
+| Test URL              | undefined |
 
 ## Example Code for chilipeppr.load() Statement
 
@@ -34,7 +34,7 @@ back the instance of it to init() it.
 // this workspace should be loaded into.
 chilipeppr.load(
   "#pnlWorkspace",
-  "http://raw.githubusercontent.com/https://master/auto-generated-workspace.html",
+  "https://raw.githubusercontent.com/chilipeppr/workspace-robot/master/auto-generated-workspace.html",
   function() {
     // Callback after workspace loaded into #pnlWorkspace
     // Now use require.js to get reference to instantiated workspace
@@ -135,7 +135,7 @@ The table below shows, in order, the methods and properties inside the workspace
           </tr>
       </thead>
       <tbody>
-      <tr valign="top"><td>id</td><td>string</td><td>"com-chilipeppr-workspace-robot"<br><br>The ID of the widget. You must define this and make it unique.</td></tr><tr valign="top"><td>name</td><td>string</td><td>"Workspace / Robot"</td></tr><tr valign="top"><td>desc</td><td>string</td><td>"This workspace is for controlling a 6 DOF robot arm."</td></tr><tr valign="top"><td>url</td><td>string</td><td>"http://raw.githubusercontent.com/https://master/auto-generated-workspace.html"</td></tr><tr valign="top"><td>fiddleurl</td><td>string</td><td>"http://ide.c9.io/undefined/undefined"</td></tr><tr valign="top"><td>githuburl</td><td>string</td><td>"http://github.com/https:/"</td></tr><tr valign="top"><td>testurl</td><td>string</td><td>"http://undefined-undefined.c9users.io/workspace.html"</td></tr><tr valign="top"><td>foreignSubscribe</td><td>object</td><td>Please see docs above.</td></tr><tr valign="top"><td>foreignPublish</td><td>object</td><td>Please see docs above.</td></tr><tr valign="top"><td>widgetConsole</td><td>object</td><td>Contains reference to the Console widget object. Hang onto the reference
+      <tr valign="top"><td>id</td><td>string</td><td>"com-chilipeppr-workspace-robot"<br><br>The ID of the widget. You must define this and make it unique.</td></tr><tr valign="top"><td>name</td><td>string</td><td>"Workspace / Robot"</td></tr><tr valign="top"><td>desc</td><td>string</td><td>"This workspace is for controlling a 6 DOF robot arm."</td></tr><tr valign="top"><td>url</td><td>string</td><td>"https://raw.githubusercontent.com/chilipeppr/workspace-robot/master/auto-generated-workspace.html"</td></tr><tr valign="top"><td>fiddleurl</td><td>string</td><td>"undefined"</td></tr><tr valign="top"><td>githuburl</td><td>string</td><td>"https://github.com/chilipeppr/workspace-robot"</td></tr><tr valign="top"><td>testurl</td><td>string</td><td>"undefined"</td></tr><tr valign="top"><td>foreignSubscribe</td><td>object</td><td>Please see docs above.</td></tr><tr valign="top"><td>foreignPublish</td><td>object</td><td>Please see docs above.</td></tr><tr valign="top"><td>widgetConsole</td><td>object</td><td>Contains reference to the Console widget object. Hang onto the reference
 so we can resize it when the window resizes because we want it to manually
 resize to fill the height of the browser so it looks clean.</td></tr><tr valign="top"><td>widgetSpjs</td><td>object</td><td>Contains reference to the Serial Port JSON Server object.</td></tr><tr valign="top"><td>init</td><td>function</td><td>function() <br><br>The workspace's init method. It loads the all the widgets contained in the workspace
 and inits them.</td></tr><tr valign="top"><td>getBillboard</td><td>function</td><td>function() <br><br>Returns the billboard HTML, CSS, and Javascript for this Workspace. The billboard
--- runme.js
+++ runme.js
@@ -177,7 +177,7 @@ var fileJsPath = "workspace.js"
 var fileCssPath = "workspace.css"
 var fileHtmlPath = "workspace.html"
 
-var widgetUrl = 'http://' +
+/*var widgetUrl = 'http://' +
     process.env.C9_PROJECT + '-' + process.env.C9_USER +
     '.c9users.io/workspace.html';
 var testUrl = 'https://preview.c9users.io/' +
@@ -188,13 +188,25 @@ var testUrlNoSsl = 'http://' + process.env.C9_PROJECT +
 var editUrl = 'http://ide.c9.io/' +
     process.env.C9_USER + '/' +
     process.env.C9_PROJECT;
-var github;
+var github;*/
+
+var urls; // = getAllUrls();
+var widgetUrl; // = urls.widgetUrl;
+var testUrl; // = urls.testUrl;
+var testUrlNoSsl; // = urls.testUrlNoSsl;
+var editUrl; // = urls.editUrl;
+var github; // = getGithubUrl();
 
 var widgetSrc, widget, id, deps, cpdefine, requirejs, cprequire_test;
 var widgetDocs = {};
 
 var init = function() {
-    github = getGithubUrl();
+  urls = getAllUrls();
+  widgetUrl = urls.widgetUrl;
+  testUrl = urls.testUrl;
+  testUrlNoSsl = urls.testUrlNoSsl;
+  editUrl = urls.editUrl;
+  github = getGithubUrl();
 }
 
 var isEvaled = false;
@@ -1358,14 +1370,20 @@ var getGithubUrl = function(callback) {
   var cmd = 'git config --get remote.origin.url';
 
   var stdout = childproc.execSync(cmd, { encoding: 'utf8' });
-  //console.log("Got the following Github URL:", stdout);
+  console.log("Got the following Github URL:", stdout);
 
   var re = /.*github.com:/i;
   var url = stdout.replace(re, "");
-  url = url.replace(/.git[\s\S]*$/i, ""); // remove end
-  
-  // prepend with clean githut url
-  url = "http://github.com/" + url;
+  url = url.replace(/.git[\s\S]{0,1}$/i, ""); // remove end
+  console.log("after removing *.git:", url);
+
+  // see if github.com already in url
+  if (url.startsWith("https://github.com") || url.startsWith("http://github.com")) {
+    // full url already in there
+  } else {
+    // prepend with clean githut url
+    url = "http://github.com/" + url;
+  }
   
   var rawurl = url.replace(/\/github.com\//i, "/raw.githubusercontent.com/");
   rawurl += '/master/' + fileAutoGeneratePath;
@@ -1375,9 +1393,183 @@ var getGithubUrl = function(callback) {
     rawurl : rawurl
   };
   
-  //console.log("ret:", ret);
+  console.log("ret:", ret);
   return ret;
     
 }
+
+var getAllUrls = function() {
+    
+  // we need to get all of these urls for either cloud9 original or AWS's new version of cloud9
+  // see what environment we're in
+  var ret = {
+      cpload: "",
+      edit: "",
+      github: "",
+      test: "",
+      testNoSsl: "",
+      runmeHomepage: "",
+  }
+  
+  var git = getGithubUrl();
+  ret.cpload = git.rawurl;
+  ret.github = git.url;
+  
+  // are we in cloud9 or aws?
+  /*
+  For new AWS Cloud9 version
+  chilipeppr.load() URL	https://raw.githubusercontent.com/chilipeppr/widget-xbox/master/auto-generated-widget.html
+  Edit URL	            https://us-west-2.console.aws.amazon.com/cloud9/ide/83c03ab3f6f9431aa813882decbfc4aa
+  Github URL	            https://github.com/chilipeppr/widget-xbox
+  Test URL	            https://vfs.cloud9.us-west-2.amazonaws.com/vfs/83c03ab3f6f9431aa813882decbfc4aa/preview/widget-xbox/widget.html
+  Test URL No SSL	
+  
+  For Original Cloud9
+  chilipeppr.load() URL   http://raw.githubusercontent.com/chilipeppr/widget-xbox/master/auto-generated-widget.html
+  Edit URL                http://ide.c9.io/chilipeppr/widget-xbox
+  Github URL              http://github.com/chilipeppr/widget-xbox
+  Test URL                https://preview.c9users.io/chilipeppr/widget-xbox/widget.html
+  Test URL No SSL	
+  */
+  if (isAwsEnvironment()) {
+    // we are in AWS
+    console.log("We are in AWS. Setting vars based on that.");
+
+    // get region
+    var region = whichAwsRegion();
+    
+    // https://us-west-2.console.aws.amazon.com/cloud9/ide/83c03ab3f6f9431aa813882decbfc4aa
+    ret.edit = 'https://' + region + '.console.aws.amazon.com/cloud9/ide/' + process.env.C9_PID;
+    // https://vfs.cloud9.us-west-2.amazonaws.com/vfs/83c03ab3f6f9431aa813882decbfc4aa/preview/widget-xbox/widget.html
+    ret.test = 'https://vfs.cloud9.' + region + '.amazonaws.com/vfs/' + 
+        process.env.C9_PID + '/preview/' + 
+        process.env.C9_PROJECT + '/widget.html';
+    // http://83c03ab3f6f9431aa813882decbfc4aa.vfs.cloud9.us-west-2.amazonaws.com/widget.html
+    ret.testNoSsl = 'http://' + process.env.C9_PID + '.vfs.cloud9.' + region + '.amazonaws.com/widget.html';
+    // http://83c03ab3f6f9431aa813882decbfc4aa.vfs.cloud9.us-west-2.amazonaws.com/
+    ret.runmeHomepage = 'https://' + process.env.C9_PID + '.vfs.cloud9.' + region + '.amazonaws.com/';
+  
+  } else if (isC9Environment()) {
+  
+    // we are in original cloud9
+    console.log("We are in C9. Setting vars based on that.");
+    // var ret.edit = 'http://' +
+    //     process.env.C9_PROJECT + '-' + process.env.C9_USER +
+    //     '.c9users.io/widget.html';
+    ret.edit = 'http://ide.c9.io/' +
+        process.env.C9_USER + '/' +
+        process.env.C9_PROJECT;
+    ret.test = 'https://preview.c9users.io/' +
+        process.env.C9_USER + '/' +
+        process.env.C9_PROJECT + '/widget.html';
+    ret.testNoSsl = 'http://' + process.env.C9_PROJECT +
+        '-' + process.env.C9_USER + '.c9users.io/widget.html';
+    // https://widget-xbox-chilipeppr.c9users.io/
+    ret.runmeHomepage = 'https://' + process.env.C9_PROJECT +
+        '-' + process.env.C9_USER + '.c9users.io/';
+  
+  } else  {
+    
+    // we are in localhost
+    console.log("We are on a local machine. Setting vars based on that.");
+    // var ret.edit = 'http://' +
+    //     process.env.C9_PROJECT + '-' + process.env.C9_USER +
+    //     '.c9users.io/widget.html';
+    ret.edit = '(Local dev. No edit URL)';
+    ret.test = 'http://localhost:9000/widget.html';
+    ret.testNoSsl = 'http://localhost:9000/widget.html';
+    // https://widget-xbox-chilipeppr.c9users.io/
+    ret.runmeHomepage = 'http://localhost:9000/';
+  }
+
+  // console.log("URLs we are using:", ret);
+  return ret;
+}
+
+var isAwsEnvironment = function() {
+  
+  // AWS cloud9 instances have AWS environment variables, so we should be able to use that
+  // to distinguish from original cloud9 to AWS's version
+  // var childproc = require('child_process');
+  // var cmd = 'env | grep AWS';
+  // var stdout = childproc.execSync(cmd, { encoding: 'utf8' });
+  var listOfEnvs = Object.keys(process.env).join(",");
+  // console.log("isCloud9OrAws:", listOfEnvs);
+  
+  if (listOfEnvs.match(/AWS/)) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+var isC9Environment = function() {
+  
+  // Cloud9 instances have C9 environment variables, so we should be able to use that
+  // to distinguish from original cloud9 to AWS's version
+  // var childproc = require('child_process');
+  // var cmd = 'env | grep C9';
+  // var stdout = childproc.execSync(cmd, { encoding: 'utf8' });
+  var listOfEnvs = Object.keys(process.env).join(",");
+  // console.log("isCloud9OrAws:", listOfEnvs);
+  
+  if (listOfEnvs.match(/C9/)) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+var whichAwsRegion = function() {
+  
+  // we can figure out the aws region by looking at the arn value
+  // arn:aws:cloudformation:us-west-2:381976811276:stack/aws-cloud9-workspace-tinyg-820f668385554da2bde72957a9078cdc/e0c47e00-f3ea-11e7-9ccf-503aca41a08d
+  // arn:aws:cloudformation:us-west-2:381976811276:stack/aws-cloud9-widget-xbox-83c03ab3f6f9431aa813882decbfc4aa/c4603630-f18a-11e7-a76d-50a686fc37d2
+  // arn:aws:cloudformation:us-east-1:381976811276:stack/awseb-e-xykh2cx2kq-stack/759041b0-12ac-11e3-9b45-50e24162947c
+  // arn:aws:cloudformation:us-east-2:381976811276:stack/aws-cloud9-widget-eagle-da906241afb9471ba097583389a735a0/d1fbd9d0-f3f5-11e7-97bb-500cef930c1e
+
+  var childproc = require('child_process');
+  var cmd = 'aws ec2 describe-instances';
+  var stdout = "";
+  var region = "";
+  try {
+      stdout = childproc.execSync(cmd, { encoding: 'utf8' });
+      // console.log("whichAwsRegion:", stdout);
+      // if we get here, we got good execution of aws command
+      if (stdout.match(/arn:aws:cloudformation:(.*?):/)) {
+          // found an arn with a region
+          region = RegExp.$1;
+      } else {
+          console.log("could not find region");
+      }
+  } catch(e) {
+      console.warn("Could not execute cmd line:", cmd);
+  }
+  
+  return region;
+}
+
+var triggerStorageOfCredentials = function() {
+  // as long as we run this cmd, the first time you enter your user/pass it will store it
+  // so each time you run this, it won't re-ask you
+  // git config credential.helper store
+  var childproc = require('child_process');
+  var cmd = 'git config credential.helper store';
+  var stdout = "";
+  try {
+      stdout = childproc.execSync(cmd, { encoding: 'utf8' });
+  } catch(e) {
+      console.warn("Could not execute cmd line:", cmd);
+  }
+}
+
+// if (isAwsEnvironment()) {
+  triggerStorageOfCredentials();
+// }
+
+var urls = getAllUrls();
+console.log("urls:", urls);
+console.log("");
+console.log("You can now view the home page of runme.js at " + urls.runmeHomepage);
 
 init();
